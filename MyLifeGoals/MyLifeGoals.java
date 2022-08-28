339a959b1c8972dcb02ef85309def1d09ba994a1
Ersin Aydoğmuş <ersinaydogmus.7@gmail.com>
2022-08-28 10:04:21 +0300

    My Life Goals
    Akdeniz University First Assignment

---
+++ MyLifeGoals/MyLifeGoals.java
@@ -0,0 +1,19 @@
+//Ersin Aydoğmuş 25.10.2020
+public class MyLifeGoals_20190808028 {
+    public static void main(String[] args) {
+
+        System.out.println(" \n Ersin Aydogmus");
+
+        System.out.println("\n In this period of distance education, " +
+                "improving my English further and being very successful " +
+                "in classes are among my short-term goals.");
+
+        System.out.println("\n My mid-term goals include learning many " +
+                "programming languages and mastering a few as well as " +
+                "getting a job.");
+
+        System.out.println("\n One of my long-term goals is to prove that I" +
+                " am a good software developer after graduation and then open" +
+                " my own company.");
+    }
+}
